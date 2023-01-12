0710e752045a67a1fe4b2dcbb3d0f8f66549854e
MOR-E <pedrohenrique88977944@gmail.com>
2023-01-12 17:41:56 -0300

    first commit

---
+++ src/Tmdb.js
@@ -0,0 +1,11 @@
+//Arquivo que lida com requições ao TMDB, uma forma de prevenir caso no futuro venha a ser necessario
+//utilizar uma outra API, basta alterar esse arquivo mantendo a mesma estrutura.
+
+const API_KEY = "f6b66bc3a1bda3d760975538662acf34";
+const API_BASE = "https://api.themoviedb.org/3";
+
+
+
+export default {
+
+}
